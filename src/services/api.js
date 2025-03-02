5de195846842b61a9f194c2111913568a3027e68
hiten-shah0106 <hiten010607shah@gmail.com>
2025-03-02 11:53:23 +0530

    Hide The API Details

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const response = await fetch(`/api/movie/popular?api_key=${API_KEY}`);
     const data = await response.json();
     return data.results;
 }
